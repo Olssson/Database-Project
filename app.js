7d5d405f69dd2149c17ff9b38395b4361bba23cc
Olsson_Polska <olson.ber@gmail.com>
2022-04-01 13:47:22 +0200

    dobra

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 //npm start
 const express = require("express");
 const mysql = require("mysql")
+const dotenv = require("dotenv");
+const path = require('path')
+// const { path } = require("express/lib/application");
+
+dotenv.config({ path: './.env'});
 
 const app = express("express");
 
 const db = mysql.createConnection({
     //w nazwie hosta jak chcem wstawić stronę na server należy zmienić nazwę z localhost na np ip
-    host: "localhost",
-    user: 'root',
-    password:'',
-    database:'login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
+// console.log(__dirname)
+const publicDirectory = path.join(__dirname)
+// const publicDirectory = __dirname + '\\views'
+console.log(publicDirectory)
+app.set('view engine', 'hbs');
 
 db.connect( (error) =>{
     if(error) {
