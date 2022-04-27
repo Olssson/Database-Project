abdc254049cd5c392353beed7cc3d7d500857110
ArtishGH <97730625+ArtishGH@users.noreply.github.com>
2022-04-27 21:26:58 +0200

    Version 1.1
    Added References

--- sql/Sql_Login.sql
+++ sql/Sql_Login.sql
@@ -1,8 +1,11 @@
 CREATE TABLE Klient (
+Id INT NOT NULL PRIMARY KEY,
 Imie VARCHAR(30) NOT NULL CHECK(LEN(Imie) > 3),
 Nazwisko VARCHAR(30) NOT NULL CHECK(LEN(Nazwisko) > 3),
 Email VARCHAR(50) NOT NULL CHECK(LEN(Email) > 7),
 DataUrodzenia DATE NOT NULL,
 Haslo VARCHAR(25) NOT NULL CHECK(LEN(Haslo) > 8),
 RodzajUzytkownika VARCHAR(7) NOT NULL CHECK(RodzajUzytkownika IN ('ADMIN', 'USER')),
+AdresId INT NOT NULL REFERENCES AdresWysylki (Id),
+KoszykId INT NOT NULL REFERENCES Koszyk (Id),
 )
