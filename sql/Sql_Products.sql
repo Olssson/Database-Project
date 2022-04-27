586ed280f42667b42a162c77596e3b8404c588ce
ArtishGH <97730625+ArtishGH@users.noreply.github.com>
2022-04-27 21:27:39 +0200

    Version 1.1
    Added References

--- sql/Sql_Products.sql
+++ sql/Sql_Products.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Produkty(
+Id INT NOT NULL PRIMARY KEY REFERENCES ProduktyKoszyk (ProduktId),
 Nazwa VARCHAR(30) NOT NULL,
 Cena INT NOT NULL,
 OgraniczenieWiekowe CHAR(3) CHECK(OgraniczenieWiekowe IN ('Tak', 'Nie')),
